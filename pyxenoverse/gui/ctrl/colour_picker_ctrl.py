eaaa34c3f3b15d3a09ef9d9eca108f73de803b6b
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-06-18 21:20:09 +0500

    fix copying of transparency's color - link the dummy RGB controls with the main color control so changing color ctrl sets the RGB too (of the dummy RGB and the actual entry's data)

--- pyxenoverse/gui/ctrl/colour_picker_ctrl.py
+++ pyxenoverse/gui/ctrl/colour_picker_ctrl.py
@@ -7,6 +7,7 @@ class ColourPickerCtrl(wx.Panel):
         kwargs["style"] = kwargs.get("style", wx.CLRP_DEFAULT_STYLE) | wx.CLRP_SHOW_LABEL
         self.sizer = wx.BoxSizer()
 
+        self.rgb_binding = kwargs.pop('rgb_binding')
         self.color_ctrl = wx.ColourPickerCtrl(self, *args, **kwargs)
 
         self.sizer.Add(self.color_ctrl, 0, wx.ALL)
@@ -19,8 +20,11 @@ class ColourPickerCtrl(wx.Panel):
         r = color.Red()
         g = color.Green()
         b = color.Blue()
+        # picking a color calls GetValue, so set the RGB dummy controls here also
+        self.rgb_binding(r,g,b)
         return r, g, b
 
     def SetValue(self, value):
         r, g, b = value
         self.color_ctrl.SetColour(wx.Colour(r, g, b))
+        self.rgb_binding(r,g,b)
